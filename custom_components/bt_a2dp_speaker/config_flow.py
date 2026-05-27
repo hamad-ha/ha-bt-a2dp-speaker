ca8eefd040a3c7a77c5efa222f21fa930f309e18
hamad-ha <hs.alsuwaidi0610@gmail.com>
2026-05-27 20:40:53 +0300

    Update config_flow.py

--- custom_components/bt_a2dp_speaker/config_flow.py
+++ custom_components/bt_a2dp_speaker/config_flow.py
@@ -107,7 +107,7 @@ class BtA2dpSpeakerOptionsFlow(config_entries.OptionsFlow):
     """Handle options for Bluetooth A2DP Speaker."""
 
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        self.config_entry = config_entry
+        self._config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
