1a4b26e4c04c0d0d1567496ecc3c42156d2f8313
hamad-ha <hs.alsuwaidi0610@gmail.com>
2026-05-27 21:01:55 +0300

    Update const.py

--- custom_components/bt_a2dp_speaker/const.py
+++ custom_components/bt_a2dp_speaker/const.py
@@ -1,26 +1,4 @@
-"""Constants for the Bluetooth A2DP Speaker integration."""
+"""Constants for Bluetooth A2DP Speaker integration."""
 
 DOMAIN = "bt_a2dp_speaker"
 PLATFORMS = ["media_player"]
-
-# Config keys
-CONF_MAC_ADDRESS = "mac_address"
-CONF_SINK_NAME = "sink_name"
-
-# Options keys
-CONF_CONNECT_ON_PLAY = "connect_on_play"
-CONF_DISCONNECT_ON_IDLE = "disconnect_on_idle"
-CONF_IDLE_TIMEOUT = "idle_timeout"
-
-# Defaults
-DEFAULT_CONNECT_ON_PLAY = True
-DEFAULT_DISCONNECT_ON_IDLE = True
-DEFAULT_IDLE_TIMEOUT = 300  # 5 minutes
-
-# Bluetooth connection states
-BT_STATE_CONNECTED = "connected"
-BT_STATE_DISCONNECTED = "disconnected"
-BT_STATE_CONNECTING = "connecting"
-
-# bluetoothctl / bluez exit codes
-BLUEZ_CONNECT_SUCCESS = 0
